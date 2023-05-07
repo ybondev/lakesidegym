e1dc02d481899faf5c4078d6723cb6688926ce49
ybondev <ybon.dev@gmail.com>
2023-05-07 10:54:02 +0800

    added aos

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const sticky_nav = document.querySelector("header");
 window.addEventListener("scroll", function () {
   sticky_nav.classList.toggle("sticky", scrollY > 100);
 });
+
+AOS.init({
+  delay: 500,
+  duration: 1000, // values from 0 to 3000, with step 50ms
+});
